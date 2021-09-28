033c568fff54313ca6f0dfdf366b9bec55c130d9
Amir Hussain <55963887+amir-hu@users.noreply.github.com>
2021-09-28 17:11:59 +0000

    Bot no longer sends fx if link is broken

--- main.py
+++ main.py
@@ -11,14 +11,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  
     if message.author == client.user:
-        return
+      try:
+        embeds = message.embeds
+        print(embeds[0].to_dict()['description'])
+        if(embeds[0].to_dict()['description']=="Failed to scan your link!"):
+          await message.delete()
+      except:
+        pass
 
+
+    if message.content.lower().startswith("cp"):
+      
+      data = message.content.split(" ")
+      newMessage = "enter in format cp <pokemon name> <iv> <filters>"
+      if(len(data) >= 3):
+        newMessage = "p!m s --n " + data[1] + " --iv >" + data[2] + " --o price+ "
+        for x in range(3,len(data)):
+          newMessage = newMessage + "--" + data[x] + " "
+      await  message.channel.send(newMessage)
+      
     if message.content.startswith("https://twitter.com"):
       
         
         newMessage = message.content.replace("https://twitter.com","https://fxtwitter.com")
-        await message.channel.send(newMessage)
+        embeds = await message.channel.send(newMessage)
+        
     
 
 keep_alive()
